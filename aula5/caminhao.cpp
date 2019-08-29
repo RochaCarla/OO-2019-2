e21701142187da42cdd3a280fa1021b1cf3e01a3
AlvaroLeles <alvaro.leles@hotmail.com>
2019-08-29 11:44:13 -0300

    Recebendo string como parametro

--- aula5/caminhao.cpp
+++ aula5/caminhao.cpp
@@ -1,11 +1,12 @@
 #include "caminhao.hpp"
 
 
-Caminhao::Caminhao()
+Caminhao::Caminhao(string marca)
 {
     carga = 0.0;
     comprimento = 0.0;
     altura = 0.0;
     cout << "Construtor do caminhao" << endl;
+    cout << "Caminhão " << marca;
 
 }
